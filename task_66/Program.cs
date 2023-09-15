77b0e2a4571bbef8ce02b32838e3c55ebd1ebf2e
Vyacheslav <asyofka@mail.ru>
2023-09-16 00:34:34 +0300

    task_66 done

--- task_66/Program.cs
+++ task_66/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Задача 66: Задайте значения M и N. Напишите программу,
+которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15-> 120
+M = 4; N = 8. -> 30
+*/
+
+//step1
+int InputNumbers(string text)
+{
+  Console.Write(text);
+  return int.Parse(Console.ReadLine()!);
+}
+
+//step2
+int count = 0;
+void PrintSumNumbersFromMToN(int m, int n)
+{
+  if (m <= n)
+  {
+    PrintSumNumbersFromMToN(m + 1, n);
+    count += m;
+  }
+}
+
+//step3
+int valueM = InputNumbers("Введите значение M: ");
+int valueN = InputNumbers("Введите значение N: ");
+PrintSumNumbersFromMToN(valueM, valueN);
+Console.Write($"M = {valueM}; N = {valueN} -> {count} ");
